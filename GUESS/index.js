03696bba3c82b7cd19aa61fcc9d7872d86b3b774
RoddickMhg <mahoungouroddick@gmail.com>
2024-10-24 15:12:21 +0200

    Debug

--- GUESS/index.js
+++ GUESS/index.js
@@ -1,18 +1,18 @@
-prompt("Mettez un nombre !")
-let  givenNumber = (prompt)
+let  givenNumber = prompt("Mettez un nombre !")
 
 function didIWin(givenNumber){
-    if (givenNumber>22){
+    alert(givenNumber)
+    if (givenNumber<22){
     alert('Plus grand')
     }
-    else if (givenNumber<22){
+    else if (givenNumber>22){
     alert('Plus petit')
     }
     else{
         alert('Bravo ! Vous avez deviné le nombre')
     }
 }
-didIWin()
+didIWin(givenNumber)
 function gamePlay(){
 
 }
