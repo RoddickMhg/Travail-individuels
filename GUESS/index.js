5ee03f8ddae0ad2b13e4aeb9ad9620743c2c0395
RoddickMhg <mahoungouroddick@gmail.com>
2024-10-24 14:32:39 +0200

    guess a number

---
+++ GUESS/index.js
@@ -0,0 +1,18 @@
+prompt("Mettez un nombre !")
+let  givenNumber = (prompt)
+
+function didIWin(givenNumber){
+    if (givenNumber>22){
+    alert('Plus grand')
+    }
+    else if (givenNumber<22){
+    alert('Plus petit')
+    }
+    else{
+        alert('Bravo ! Vous avez deviné le nombre')
+    }
+}
+didIWin()
+function gamePlay(){
+
+}
